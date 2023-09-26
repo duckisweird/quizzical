228411d4379a17f93134cc30d81b6df424b79d88
August <87242744+duckisweird@users.noreply.github.com>
2023-09-26 17:52:13 +0200

    Update Quiz.js

--- src/Pages/Quiz.js
+++ src/Pages/Quiz.js
@@ -1,51 +1,86 @@
 import React, {useState, useEffect} from "react";
 
 import { decode } from "html-entities"
+import { nanoid } from "nanoid"
 
 export default function Quiz() {
 
  
 
     const [quizData, setQuizData] = useState([])
+    const [reset, setReset] = useState([0])
 
     useEffect(() => {
         fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-        .then((res) => res.json())
-        .then((data) => setQuizData(data.results))  
-    }, [])
+                .then((response) => response.json())
+                .then((data) => {
+                    let resultArray = [];
+                    data.results.map((result) => {
+                        return resultArray.push({
+                            id: nanoid(),
+                            question: result.question,
+                            correct_answer: result.correct_answer,
+                            answers: result.incorrect_answers
+                                .concat(result.correct_answer)
+                                .sort(
+                                    () => Math.random() - 0.5
+                                ),
+                            isHeld: false,
+                            selectedAnswer: "",
+                            
+                        });
+                    });
+                    setQuizData(resultArray)
+                });
+    }, [reset])
 
 
-    const buttonData = quizData.map(opt => {
-        const optionData = opt.incorrect_answers.concat(opt.correct_answer).toString()
-        const shuffledOptionData = optionData.sort(() => (Math.random() > .5) ? 1 : -1)
+    function holdAnswer(quesID, ansID) { 
+       setQuizData(prevData => {
+        return prevData.map(quiz => {
+            if (quiz.id === quesID) {
+                return {...quiz, allAnswers: quiz.allAnswers.map(ans => {
+                    return ans.id === ansID ? {...ans, isChecked: !ans.isChecked} : {...ans, isChecked: false}
+                })}
+            } else return quiz
+        })
+       })
+    }
+
+    const elementData = quizData.map((quest) => {
+        
+        
 
         return (
-            <button className="quiz--btn">{decode(shuffledOptionData)}</button>
-        )
+            <div key={quest.id}>
+                <div className="quiz--question">{decode(quest.question)}</div>
+                
+                    
+                        <ul className="quiz--options">
+                            {quest.answers.map((ans) => {
+                                return (
+                                <li
+                                key={decode(ans)}
+                                id={decode(ans)}
+                                className={quest.isHeld ? "quiz--selected" : "quiz--opt"}
+                                onClick={() => holdAnswer(quest.question, ans)}
+                                >
+                                    {decode(ans)}
+                                </li>
+                                )
+                            })}
+                        </ul>
+                
+            </div>
+            
+            )
     })
     
     return (
         <div className="quiz">
-            {quizData.map(arr => {
-                return (
-                    <div>
-                        <div className="quiz--question">{decode(arr.question)}</div>
-                        
-                    </div>
-                    )})}
-
-                   
-                
-        
-        {buttonData}
-                    
-                    
-                            
-                        
-                    
+                {elementData}     
         </div>
     
         
     )
 }
-
