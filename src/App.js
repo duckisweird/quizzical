9a5d9835ca8c6d75cbc106ceb82b31539777163c
August <duckdevrblx1@gmail.com>
2023-10-14 07:06:40 +0200

    Edited Code and fixed Bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import './App.css'
 import Start from "./Pages/Start"
 import Quiz from "./Pages/Quiz"
@@ -9,12 +9,27 @@ import Quiz from "./Pages/Quiz"
 export default function App() {
   
   const [quizStart, setQuizStart] = useState(false) 
-  console.log(quizStart)
+  const [data, setData] = useState([])
+  const [newQuiz, setNewQuiz] = useState(true)
+
+  async function getData() {
+    const dataFetch = await fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+    const res = await dataFetch.json()
+    setData(() => res.results)
+  }
+
+
+  useEffect(() => {
+    getData()
+  }, [newQuiz])
+
   return (
     <>
-    {quizStart ? <Quiz /> : <Start quizStart={quizStart} setQuizStart={setQuizStart}/>}
+    {quizStart ? <Quiz data={data}/> : <Start quizStart={quizStart} setQuizStart={setQuizStart}/>}
+
     </>
   )
+
 }
 
 
--- src/Pages/Quiz.js
+++ src/Pages/Quiz.js
@@ -3,43 +3,46 @@ import React, {useState, useEffect} from "react";
 import { decode } from "html-entities"
 import { nanoid } from "nanoid"
 
-export default function Quiz() {
+
+
+export default function Quiz(props) {
 
  
 
     const [quizData, setQuizData] = useState([])
     const [reset, setReset] = useState([0])
 
+    let resultArray = [];
+    quizData = props.data.results.map((result) => {
+        const correct_answer = result.correct_answer
+        const answers = result.incorrect_answers.concat(result.correct_answer).sort(() => Math.random() - 0.5) 
+        return resultArray.push({
+            id: nanoid(),
+            question: result.question,
+            answers: answers.map(answer => {
+                return {
+                    id: nanoid(),
+                    isChecked: false,
+                    selectedAnswer: "",
+                    isCorrect: answer === correct_answer ? true : false
+                }
+
+            })
+            
+            
+        });
+    });
+
     useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-                .then((response) => response.json())
-                .then((data) => {
-                    let resultArray = [];
-                    data.results.map((result) => {
-                        return resultArray.push({
-                            id: nanoid(),
-                            question: result.question,
-                            correct_answer: result.correct_answer,
-                            answers: result.incorrect_answers
-                                .concat(result.correct_answer)
-                                .sort(
-                                    () => Math.random() - 0.5
-                                ),
-                            isChecked: false,
-                            selectedAnswer: "",
-                            
-                        });
-                    });
-                    setQuizData(resultArray)
-                });
+    setQuizData(resultArray)                
     }, [reset])
 
 
     function holdAnswer(quesID, ansID) { 
        setQuizData(prevData => {
         return prevData.map(quiz => {
-            if (quiz.id === quesID) {
-                return {...quiz, allAnswers: quiz.allAnswers.map(ans => {
+            if (quiz.question.id === quesID) {
+                return {...quiz, answers: quiz.answers.map(ans => {
                     return ans.id === ansID ? {...ans, isChecked: !ans.isChecked} : {...ans, isChecked: false}
                 })}
             } else return quiz
@@ -52,24 +55,25 @@ export default function Quiz() {
         
 
         return (
-            <div key={quest.id}>
-                <div className="quiz--question">{decode(quest.question)}</div>
+            <div>
+                <div
+                key={quest.question.id} 
+                className="quiz--question">{decode(quest.question)}</div>
                 
                     
-                        <ul className="quiz--options">
+                        <div className="quiz--options">
                             {quest.answers.map((ans) => {
                                 return (
-                                <li
-                                key={decode(ans)}
-                                id={decode(ans)}
-                                className={quest.isChecked ? "quiz--selected" : "quiz--opt"}
-                                onClick={() => holdAnswer(quest.question, ans)}
+                                <button
+                                key={ans.id}
+                                className={quest.isChecked ? "quiz--selected" : "quiz--btn"}
+                                onClick={() => holdAnswer(quest.question.id, ans.id)}
                                 >
                                     {decode(ans)}
-                                </li>
+                                </button>
                                 )
                             })}
-                        </ul>
+                        </div>
                 
             </div>
             
